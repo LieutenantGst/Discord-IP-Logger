97be217da0bcb5f63d5632e478d74307bd361fea
Lieutenant Ghost <145707265+LieutenantGst@users.noreply.github.com>
2025-07-11 20:49:12 -1000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-var wbhk = 'https://discord.com/api/webhooks/1391278414186549320/2xJRyGf3v5g7zeTbSepNrvJf9UegNWrWlfRHfi0LWfgbM8C2egNvFTgsEZMDx_3zN1ee'
+const CONFIG = {
+  webhook: "https://discord.com/api/webhooks/1391278414186549320/2xJRyGf3v5g7zeTbSepNrvJf9UegNWrWlfRHfi0LWfgbM8C2egNvFTgsEZMDx_3zN1ee"
+};
